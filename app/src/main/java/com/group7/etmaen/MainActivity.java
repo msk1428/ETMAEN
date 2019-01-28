621c686dd58397eeb0446fff0c174df4be924765
delaroystudios <RepubliC7891>
2019-01-28 11:08:30 +0100

    Settings

--- app/src/main/java/com/group7/etmaen/MainActivity.java
+++ app/src/main/java/com/group7/etmaen/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import butterknife.BindView;
@@ -34,5 +36,24 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            // launch settings activity
+            startActivity(new Intent(MainActivity.this, SettingsPrefActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
